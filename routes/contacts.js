c281f05d588e2e3af3073163f40ed18016e34093
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-03-18 17:34:15 -0700

    add validation and check

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const contactController = require("../controllers/contacts");
+const { checkRules, validate } = require("../util/validate1");
 
 router.get("/", contactController.getAll);
 router.get("/:id", contactController.getOne);
-router.post("/", contactController.createContact);
-router.put("/:id", contactController.updateContact);
+router.post("/", checkRules, validate, contactController.createContact);
+router.put("/:id", checkRules, validate, contactController.updateContact);
 router.delete("/:id", contactController.deleteContact);
 
 module.exports = router;
