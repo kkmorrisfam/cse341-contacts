981291944a73bdad458844fbe3ee6765d2989b4f
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-03-18 18:24:01 -0700

    Validate a different way

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,12 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 const contactController = require("../controllers/contacts");
-const { checkRules, validate } = require("../util/validate1");
+
+const validate = require("../util/validate");
 
 router.get("/", contactController.getAll);
 router.get("/:id", contactController.getOne);
-router.post("/", checkRules, validate, contactController.createContact);
-router.put("/:id", checkRules, validate, contactController.updateContact);
+
+router.post(
+  "/",
+  validate.checkRules(),
+  validate.checkErrors,
+  contactController.createContact
+);
+router.put(
+  "/:id",
+  validate.checkRules(),
+  validate.checkErrors,
+  contactController.updateContact
+);
+
 router.delete("/:id", contactController.deleteContact);
+
+//version 1 - this works, but checkRules as just an array and can cause problems sometimes if data in array is changed.
+// Safer to have a function that returns array each time it's called.
+
+// const { checkRules, validate } = require("../util/validate1");
+// router.post("/", checkRules, validate, contactController.createContact);
+//router.put("/:id", checkRules, validate, contactController.updateContact);
 
 module.exports = router;
---
+++ util/validate.js
@@ -0,0 +1,33 @@
+const { check, validationResult } = require("express-validator");
+
+const validate = {};
+
+validate.checkRules = () => {
+  return [
+    check("firstName").trim().notEmpty().withMessage("First name is required"),
+    check("lastName").trim().notEmpty().withMessage("Last name is required"),
+    check("email").trim().isEmail().withMessage("Invalid email addrses"),
+    check("favoriteColor")
+      .trim()
+      .notEmpty()
+      .withMessage("Favorite Color is required"),
+    check("birthday")
+      .trim()
+      .notEmpty()
+      // .matches(/^\d{2}-\d{2}-\d{4}$/)
+      .withMessage("Birthday must be in MM-DD-YYYY format"),
+  ];
+};
+
+validate.checkErrors = (req, res, next) => {
+  // console.log("inside validate");
+  const errors = validationResult(req);
+  //if errors is not empty
+  if (!errors.isEmpty()) {
+    //status 400 = Bad Request
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+module.exports = validate;
